64a172ca87b62b0af3a59296ca863144a25cd5f8
DhruvKauts2 <kautsdhruv15@gmail.com>
2021-11-10 03:36:59 +0300

    what the actual ass

--- bert_summary.py
+++ bert_summary.py
@@ -4,11 +4,12 @@ from newspaper import fulltext
 import requests
 import sys
 import re
-article_url=str(sys.argv[1])
-article = fulltext(requests.get(article_url).text)
+#article_url=str(sys.argv[1])
+#article = fulltext(requests.get(article_url).text)
  
-bert_model = Summarizer()
-bert_summary = ''.join(bert_model(article, min_length = 100, max_length = 200))  #summary length is 10% of article length
-final_bert_summary = re.sub("[[@*&?].*[]@*&?]", "", bert_summary)
+#bert_model = Summarizer()
+#bert_summary = ''.join(bert_model(article, min_length = 100, max_length = 200))  
+#final_bert_summary = re.sub("[[@*&?].*[]@*&?]", "", bert_summary)
+bert_summary = 'Lol dude'
 print(bert_summary)
 sys.stdout.flush()
