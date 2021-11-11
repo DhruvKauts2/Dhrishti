448749ee61fe6506e7449fb3fd4c3a6a658f1677
DhruvKauts2 <kautsdhruv15@gmail.com>
2021-11-11 15:03:44 +0300

    atlas-heroku

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ const { spawn } = require('child_process');
 const { resolve } = require('path')
 const { get } = require('http')
 const DB = 'mongodb+srv://Kauts:balenciaga@dhrishtidb.06jmq.mongodb.net/dhrishtiDataBase?retryWrites=true&w=majority'
-
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
-//lol
-const cors = require("cors");
+const cors = require('cors');
 const corsOptions = {
   origin: '*',
   credentials: true,
@@ -28,6 +27,8 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then((
 app.use(cors(corsOptions))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.all('/:tbs', async (req,res) => {
     const {tbs} = req.params
@@ -45,19 +46,38 @@ app.all('/:tbs', async (req,res) => {
     
 })
 
-const ask_for_summary = async(search_key) =>  {
-  var final_summary = await get_summary(search_key)
-  const record = {keyword: search_key, summary: final_summary}
+app.post('/api/stuff', async(req, res, next) => {
+  var token = req.body["token"];
+  var text = req.body["text"];
+  console.log(token);
+  //console.log(text);
+
+  const records = await Todo.findOne({keyword: token})
+    if(records == null || records.length == 0 ){
+      
+        var final_summary = await ask_for_summary(token,text)
+        res.send(`${final_summary}`)
+    }
+    else{
+      res.send(records.summary)
+    }
+
+  res.status(201).send('Created Succesfully');
+});
+
+const ask_for_summary = async(search_token, search_text) =>  {
+  var final_summary = await get_summary(search_text)
+  const record = {keyword: search_token, summary: final_summary}
   const response = await Todo.create(record)
   console.log(response)
   return final_summary
 }
 
 
-const get_summary = async(search_key) =>{ //this function takes the keyword as parameter and returns the summary
+const get_summary = async(search_text) =>{ //this function takes the keyword as parameter and returns the summary
    var summary;
   
-   const childPython = await spawn('python', ['bert_summary.py', 'https://en.wikipedia.org/wiki/' + search_key]);
+   const childPython = await spawn('python', ['bert_summary.py', search_text]);
  
    childPython.stdout.on('data', (data) => {
        summary = data;
