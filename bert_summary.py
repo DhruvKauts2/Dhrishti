1cd2393701d276179d2518072a088b9448580fb4
DhruvKauts2 <kautsdhruv15@gmail.com>
2021-11-13 01:32:05 +0300

    lol

--- bert_summary.py
+++ bert_summary.py
@@ -6,5 +6,5 @@ import re
 article=str(sys.argv[1])
 bert_model = Summarizer()
 bert_summary = ''.join(bert_model(article, min_length = 100, max_length = 200))  #summary length is 10% of article length
-print('Lol dude')
+print(bert_summary)
 sys.stdout.flush()
