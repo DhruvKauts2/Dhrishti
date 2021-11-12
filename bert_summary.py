73de2da77de203f13c9e0507057e0dff28e963ed
DhruvKauts2 <kautsdhruv15@gmail.com>
2021-11-13 01:18:48 +0300

    test

--- bert_summary.py
+++ bert_summary.py
@@ -1,11 +1,10 @@
-  
-from summarizer import Summarizer, TransformerSummarizer
-from newspaper import fulltext
-import requests
+#from summarizer import Summarizer, TransformerSummarizer
+#from newspaper import fulltext
+#import requests
 import sys
-import re
-article=str(sys.argv[1])
-bert_model = Summarizer()
-bert_summary = ''.join(bert_model(article, min_length = 100, max_length = 200))  #summary length is 10% of article length
-print(bert_summary)
+#import re
+#article=str(sys.argv[1])
+#bert_model = Summarizer()
+#bert_summary = ''.join(bert_model(article, min_length = 100, max_length = 200))  #summary length is 10% of article length
+print('Lol dude')
 sys.stdout.flush()
